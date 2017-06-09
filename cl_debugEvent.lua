122194a6b6bc3139fd387261b69b0db72343f5eb
samuelds <samuelds.pro@gmail.com>
2017-06-09 18:51:52 +0200

    Add lines

--- cl_debugEvent.lua
+++ cl_debugEvent.lua
@@ -2,7 +2,7 @@
 -- @Date:   2017-06-08T23:28:39+02:00
 -- @Project: FiveM Tools
 -- @Last modified by:
--- @Last modified time: 2017-06-09T13:45:17+02:00
+-- @Last modified time: 2017-06-09T18:46:59+02:00
 -- @License: GNU General Public License v3.0
 
 -- get all item
@@ -19,11 +19,13 @@ end)
 RegisterNetEvent('ft_debugPlayer:ClPos')
 AddEventHandler('ft_debugPlayer:ClPos', function(name)
   Citizen.CreateThread(function()
+
     local player = GetPlayerPed(-1)
     local pos = GetEntityCoords(player)
 
     local string = "{ x = " .. pos.x .. ", y = " .. pos.y .. ", z = " .. pos.z .. ", name = '" .. name .. "' },\n"
     TriggerEvent('chatMessage', 'SYSTEM', {255, 0, 0}, "x:" .. pos.x .. " - y:" .. pos.y .. " - z:" .. pos.z)
     TriggerServerEvent('ft_debugPlayer:SvSaveToFile', string, "pos")
+
   end)
 end)
--- sv_debugEvents.lua
+++ sv_debugEvents.lua
@@ -2,18 +2,15 @@
 -- @Date:   2017-06-08T23:28:39+02:00
 -- @Project: FiveM Tools
 -- @Last modified by:
--- @Last modified time: 2017-06-09T13:49:21+02:00
+-- @Last modified time: 2017-06-09T18:36:57+02:00
 -- @License: GNU General Public License v3.0
-
--- Import lib
-require "resources/ft_gamemode/sv_utils"
 
 -- Allows the server to spawn the player
 RegisterServerEvent('ft_debug:SvDebug')
-AddEventHandler('ft_debug:SvDebug', function(data)
+AddEventHandler('ft_debug:SvDebug', function()
 
   print("--------- debug ------------")
-  tprint(data)
+  -- tprint(data)
   print("-----------------------------")
 
 end)
