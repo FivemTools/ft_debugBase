6a64a715f10ded8ff26063bd129f6d84120ecbd4
samuelds <samuelds.pro@gmail.com>
2017-06-09 13:49:34 +0200

    Update 0.3

--- sv_debugEvents.lua
+++ sv_debugEvents.lua
@@ -2,7 +2,7 @@
 -- @Date:   2017-06-08T23:28:39+02:00
 -- @Project: FiveM Tools
 -- @Last modified by:
--- @Last modified time: 2017-06-09T13:47:45+02:00
+-- @Last modified time: 2017-06-09T13:49:21+02:00
 -- @License: GNU General Public License v3.0
 
 -- Import lib
@@ -13,11 +13,7 @@ RegisterServerEvent('ft_debug:SvDebug')
 AddEventHandler('ft_debug:SvDebug', function(data)
 
   print("--------- debug ------------")
-  if debug ~= nil then
-    tprint(data)
-  else
-    print("Empty")
-  end
+  tprint(data)
   print("-----------------------------")
 
 end)
